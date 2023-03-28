4f7b5ec7d143b9abd6538d553d41224cbe823823
Thawane <thawaneapoliveira@gmail.com>
2023-03-28 15:31:11 -0300

    feat: adiciona requisito 1, migration e model para tabela teams

---
+++ app/backend/src/database/models/Teams.ts
@@ -0,0 +1,40 @@
+import { INTEGER, STRING, Model } from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class Teams extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+Teams.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  teamName: {
+    allowNull: false,
+    type: STRING,
+    field: 'team_name',
+  } }, {
+  // ... Outras configs
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
+
+export default Teams;
